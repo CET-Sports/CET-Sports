c3372e4a149ac1bb08c5905008def7e7b620cefc
Dilshad-KK <dilshadkk287@gmail.com>
2020-11-26 20:26:37 +0530

    tournament update

--- Screens/Admin/Tournament/AddTournament/addTournament.js
+++ Screens/Admin/Tournament/AddTournament/addTournament.js
@@ -5,6 +5,7 @@ import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { firebase } from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import { colors } from '../../../../Colors/colors';
 
 function addTournament(props) {
 
@@ -15,35 +16,49 @@ function addTournament(props) {
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
     const [name, setName] = useState('');
-
+    const [dept, setDept] = useState('');
+    // const [Cricket, setCricket] = useState('');
+    // const [Football, setFootball] = useState('');
 
     const onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         setShow(Platform.OS === 'ios');
         setDate(currentDate);
-      };
-    
-      const showMode = (currentMode) => {
+    };
+
+    const showMode = (currentMode) => {
         setShow(true);
         setMode(currentMode);
-      };
-    
-      const showDatepicker = () => {
+    };
+
+    const showDatepicker = () => {
         showMode('date');
-      };
-    
-      const showTimepicker = () => {
+    };
+
+    const showTimepicker = () => {
         showMode('time');
-      };
+    };
 
-      const sendData = ()=>{
-
+    const sendData = () => {
         firebase.firestore().collection('Tournaments').doc(name).set({
-            name:name,
-            dew:date,
-            type:selectedValue
+            name: name,
+            dew: date,
+            type: selectedValue,
+            status: 'Pending',
+            dept:dept
         })
-      }
+
+        if (toggleCheckBox == true) {
+            firebase.firestore().collection('Tournaments').doc(name).collection('Games').doc().set({
+                name: 'Cricket'
+            })
+        }
+        if (toggleCheckBox1 == true) {
+            firebase.firestore().collection('Tournaments').doc(name).collection('Games').doc().set({
+                name: 'Football'
+            })
+        }
+    }
 
 
 
@@ -53,7 +68,7 @@ function addTournament(props) {
             <TextInput
                 placeholder="Tournament Name"
                 style={{ marginHorizontal: 20, marginTop: 20 }}
-                onChangeText = {(value)=> setName(value)}
+                onChangeText={(value) => setName(value)}
             />
             <Text style={{ marginLeft: 20, marginTop: 20 }}>Select games</Text>
             <View style={{ flexDirection: 'row' }}>
@@ -78,7 +93,7 @@ function addTournament(props) {
 
             <Picker
                 selectedValue={selectedValue}
-                style={{ height: 50, width: 150,marginLeft:20 }}
+                style={{ height: 50, width: 150, marginLeft: 20 }}
                 onValueChange={(itemValue, itemIndex) => setSlectedValue(itemValue)}
             >
                 <Picker.Item label="Select" value="" />
@@ -87,6 +102,17 @@ function addTournament(props) {
                 <Picker.Item label="College Sports" value="Csports" />
 
             </Picker>
+
+            {
+                selectedValue == 'dept' ?
+                    <View>
+                        <TextInput placeholder="Department" style={{ marginLeft: 20 }} 
+                        onChangeText = {(value)=>setDept(value)}
+                        />
+                    </View>
+                    :
+                    null
+            }
 
             {show && (
                 <DateTimePicker
@@ -98,9 +124,13 @@ function addTournament(props) {
                     onChange={onChange}
                 />
             )}
-            <TouchableOpacity style={{marginLeft:20}} onPress={()=>{showDatepicker()}}><Text>Dew date</Text></TouchableOpacity>
-            
-            <TouchableOpacity onPress={()=>{sendData()}}><Text>Add</Text></TouchableOpacity>
+            <TouchableOpacity style={{ marginLeft: 20 }} onPress={() => { showDatepicker() }}><Text>Dew date</Text></TouchableOpacity>
+
+            <TouchableOpacity style={{ margin: 20 }} onPress={() => { sendData() }}>
+                <View style={{padding:15,backgroundColor:colors.primaryColor,borderRadius:12,marginTop:20}}>
+                <Text style={{alignSelf:'center',color:'#fff',fontSize:20}}>Add</Text>
+                </View>
+            </TouchableOpacity>
 
         </View>
     );
