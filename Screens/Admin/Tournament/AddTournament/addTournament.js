e2f8b294a8ed580503d745cbc67d894e02e72485
Dilshad-KK <dilshadkk287@gmail.com>
2020-11-27 00:11:54 +0530

    tournament updates

--- Screens/Admin/Tournament/AddTournament/addTournament.js
+++ Screens/Admin/Tournament/AddTournament/addTournament.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity, Modal, ScrollView } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { firebase } from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import { colors } from '../../../../Colors/colors';
+import styles from './styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 function addTournament(props) {
 
@@ -17,6 +20,7 @@ function addTournament(props) {
     const [show, setShow] = useState(false);
     const [name, setName] = useState('');
     const [dept, setDept] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
     // const [Cricket, setCricket] = useState('');
     // const [Football, setFootball] = useState('');
 
@@ -45,7 +49,7 @@ function addTournament(props) {
             dew: date,
             type: selectedValue,
             status: 'Pending',
-            dept:dept
+            dept: dept
         })
 
         if (toggleCheckBox == true) {
@@ -58,56 +62,64 @@ function addTournament(props) {
                 name: 'Football'
             })
         }
+
+        setModalVisible(true);
     }
 
 
 
     return (
-        <View>
+        <ScrollView contentContainerStyle={styles.container}>
 
             <TextInput
                 placeholder="Tournament Name"
-                style={{ marginHorizontal: 20, marginTop: 20 }}
+                style={styles.TextInput}
                 onChangeText={(value) => setName(value)}
             />
-            <Text style={{ marginLeft: 20, marginTop: 20 }}>Select games</Text>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center', margin: 20 }}>
-                    <Text>Football</Text>
-                    <CheckBox
-                        disabled={false}
-                        value={toggleCheckBox}
-                        onValueChange={(newValue) => setToggleCheckBox(newValue)}
-                    />
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center', margin: 20 }}>
-                    <Text>Cricket</Text>
-                    <CheckBox
-                        disabled={false}
-                        value={toggleCheckBox1}
-                        onValueChange={(newValue) => setToggleCheckBox1(newValue)}
-                    />
+
+            <View style={{ backgroundColor: '#fff' }}>
+                <Text style={{ marginLeft: 20, marginTop: 20 }}>Select games</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', margin: 20 }}>
+                        <Text>Football</Text>
+                        <CheckBox
+                            disabled={false}
+                            value={toggleCheckBox}
+                            onValueChange={(newValue) => setToggleCheckBox(newValue)}
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', margin: 20 }}>
+                        <Text>Cricket</Text>
+                        <CheckBox
+                            disabled={false}
+                            value={toggleCheckBox1}
+                            onValueChange={(newValue) => setToggleCheckBox1(newValue)}
+                        />
+                    </View>
                 </View>
             </View>
 
+            <View style={styles.pickerBox}>
+                <Picker
+                    selectedValue={selectedValue}
+                    style={{ height: 50 }}
+                    onValueChange={(itemValue, itemIndex) => setSlectedValue(itemValue)}
+                    itemStyle={{ height: 70, color: colors.primaryColor, fontSize: 40 }}
+                >
+                    <Picker.Item label="Select" value="" />
+                    <Picker.Item label="Department" value="dept" />
+                    <Picker.Item label="Inter Department" value="Idept" />
+                    <Picker.Item label="College Sports" value="Csports" />
 
-            <Picker
-                selectedValue={selectedValue}
-                style={{ height: 50, width: 150, marginLeft: 20 }}
-                onValueChange={(itemValue, itemIndex) => setSlectedValue(itemValue)}
-            >
-                <Picker.Item label="Select" value="" />
-                <Picker.Item label="Department" value="dept" />
-                <Picker.Item label="Inter Department" value="Idept" />
-                <Picker.Item label="College Sports" value="Csports" />
+                </Picker>
+            </View>
 
-            </Picker>
 
             {
                 selectedValue == 'dept' ?
                     <View>
-                        <TextInput placeholder="Department" style={{ marginLeft: 20 }} 
-                        onChangeText = {(value)=>setDept(value)}
+                        <TextInput placeholder="Department" style={{ marginLeft: 20 }}
+                            onChangeText={(value) => setDept(value)}
                         />
                     </View>
                     :
@@ -124,15 +136,48 @@ function addTournament(props) {
                     onChange={onChange}
                 />
             )}
-            <TouchableOpacity style={{ marginLeft: 20 }} onPress={() => { showDatepicker() }}><Text>Dew date</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.dateBtn} onPress={() => { showDatepicker() }}>
+                <Text style={{ color: '#fff', fontSize: 16 }}>Dew date</Text>
+            </TouchableOpacity>
 
             <TouchableOpacity style={{ margin: 20 }} onPress={() => { sendData() }}>
-                <View style={{padding:15,backgroundColor:colors.primaryColor,borderRadius:12,marginTop:20}}>
-                <Text style={{alignSelf:'center',color:'#fff',fontSize:20}}>Add</Text>
+                <View style={{ padding: 15, backgroundColor: colors.primaryColor, borderRadius: 12, marginTop: 20 }}>
+                    <Text style={{ alignSelf: 'center', color: '#fff', fontSize: 16 }}>Add Tournament</Text>
                 </View>
             </TouchableOpacity>
 
-        </View>
+
+            {
+                modalVisible ?
+
+                    <Modal
+                        animationType="fade"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => { setModalVisible(false) }}
+                    >
+
+                        <TouchableOpacity style={styles.modalContainer} onPress={() => { setModalVisible(false) }}>
+                            <View style={styles.modalView}>
+                                <View style={{ flexDirection: 'column', alignItems: 'center', width: 100, margin: 10 }}>
+                                    <Icon name="check-circle" size={60} color='#2ed573' />
+                                    <Text style={styles.modalText}>Done</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+
+                    </Modal>
+
+                    :
+
+                    null
+
+            }
+
+
+
+
+        </ScrollView>
     );
 }
 
---
+++ Screens/Admin/Tournament/AddTournament/styles.js
@@ -0,0 +1,53 @@
+
+import { StyleSheet } from 'react-native';
+import { colors } from '../../../../Colors/colors';
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#fff',
+        flex:1
+    },  
+    pickerBox:{
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#dcdee0',
+        borderRadius: 10,
+        height: 55,
+        marginVertical: 30,
+        marginHorizontal: 30,
+        backgroundColor: '#FAFAFA'
+    },
+    dateBtn:{
+        padding:15,
+        alignItems:'center',
+        marginHorizontal:20,
+        backgroundColor:'#c8d6e5',
+        borderRadius:12,
+        marginTop:20
+        
+    },
+    TextInput:{
+        borderBottomWidth:1.5,
+        borderColor:colors.primaryColor,
+        fontSize:16,
+        marginHorizontal:10,
+        marginVertical:20
+    },
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(52,52,52,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalView: {
+        backgroundColor: '#fff',
+        marginHorizontal: 20,
+        borderRadius: 14,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30
+    },
+})
+
+export default styles;
