a89ae25999e85f5c99ae6ed7216abe40149438aa
rijinaniyeri <aniyeririjin@gmail.com>
2021-01-19 12:51:12 +0530

    18-01-22...Apply

--- Screens/Admin/Invite/inviteMain.js
+++ Screens/Admin/Invite/inviteMain.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
-function inviteMain({ navigation }) {
+function inviteMain(navigation) {
     return (
         <View>
             <TouchableOpacity onPress={() => { navigation.navigate('Invite') }}>
@@ -9,6 +9,9 @@ function inviteMain({ navigation }) {
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { navigation.navigate('ViewApplication') }}>
                 <Text>VIEW COLLEGE TEAM REQUESTS</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { navigation.navigate('ViewApplication') }}>
+                <Text>APPROVED APPLICATIONS</Text>
             </TouchableOpacity>
         </View>
     );
