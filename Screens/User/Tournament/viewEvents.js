372413a4712d7587ac58cd1b72b8d13f33437a6d
rijinaniyeri <aniyeririjin@gmail.com>
2021-01-19 23:17:09 +0530

    tournament no change

--- Screens/User/Tournament/viewEvents.js
+++ Screens/User/Tournament/viewEvents.js
@@ -1,36 +1,55 @@
-import React from 'react';
-import { View, FlatList, TouchableOpacity } from 'react-native';
-import { firebase } from '@react-native-firebase/app';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, FlatList, ScrollView } from 'react-native';
+import styles from './styles';
+import { firebase } from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
-
-
-function viewEvents(props) {
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+function scores({navigation}) {
 
     const [dataSource, setDataSource] = useState([]);
 
+    useEffect(() => {
+        firebase.firestore().collection('Tournaments').onSnapshot(querySnapshot=>{
+            setDataSource([]);
+            const array = [];
+
+                firebase.firestore().collection('Tournaments').where('status','==','Ongoing').get().then(querySnapShot => {
+                    if (querySnapShot != null) {
+                        querySnapShot.forEach(documentSnapshot => {
+                            console.log('documentSnapshot.data()')
+                            console.log(documentSnapshot.data())
+                            array.push({
+                                ...documentSnapshot.data()
+                            });
+                            setDataSource(array);
+                        })
+                    }
+                })
+        })
+
+        
+
+    }, [])
+
     function Item({ data }) {
         return (
-            <View>
-                <Text></Text>
-                {/* add event name from database above */}
-                <TouchableOpacity>
-                    <Text>ENTER</Text>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.gameBtn} onPress={()=>{navigation.navigate('userGames',{
+                Tname:data.name
+            })}}>
+                <Text >{data.name}</Text>
 
-            </View>
+            </TouchableOpacity>
         )
     }
 
+
     return (
-        <View>
             <FlatList
                 data={dataSource}
                 renderItem={({ item }) => <Item data={item} />}
                 keyExtractor={(item, index) => index.toString()}
             />
-
-        </View>
     );
 }
 
-export default viewEvents;
+export default scores;
