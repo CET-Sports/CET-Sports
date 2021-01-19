bc32534c1441c44d6afd7fb7116b255410134802
Dilshad-KK <dilshadkk287@gmail.com>
2021-01-19 13:04:35 +0530

    news feed completed

--- Screens/User/Feed/styles.js
+++ Screens/User/Feed/styles.js
@@ -1,37 +1,49 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { colors } from '../../../Colors/colors';
 
- const styles = StyleSheet.create({
-feedImage:{
-    height:400,
-},
-title:{
-    marginHorizontal:10,
-    marginVertical:8,
-    lineHeight:25,
-    fontFamily:'Roboto-Regular',
-    fontSize:20,
-    color:'#000',
-    opacity:5
-},
-content:{
-    marginHorizontal:8,
-    marginBottom:13,
-    fontFamily:'Roboto-Light',
-    fontSize:17,
-    color:'#000',
-    opacity:8,
-    lineHeight:28
-},
-divider:{
-    height:1,
-    opacity:0.3,
-    backgroundColor:'#CCCCCC'
-},
-Container:{
-    backgroundColor:'#fff',
-    justifyContent:'center',
-}
+const styles = StyleSheet.create({
+    feedImage: {
+        height: 400,
+    },
+    title: {
+        marginTop:5,
+        marginHorizontal:8,
+        fontSize: 17,
+        fontFamily: 'OpenSans-SemiBold',
+        color: '#2f3640'
+    },
+    date: {
+        marginVertical:4,
+        marginHorizontal:8,
+        fontSize: 13,
+        color: 'grey',
+        fontFamily: 'OpenSans-Regular',
+    },
+    content: {
+        marginHorizontal:8,
+        fontFamily: 'Roboto-Light',
+        fontSize: 17,
+        color: '#000',
+        opacity: 8,
+        lineHeight: 28
+    },
+    divider: {
+        backgroundColor: '#40739e',
+        height: 0.4,
+        marginTop: 15
+    },
+    Container: {
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    modalView: {
+        flexGrow: 1,
+        paddingBottom:10
+    },
+    modalContainer: {
+        flex: 1,
+        marginTop:30
+    }
 })
 
 export default styles;
