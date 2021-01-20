14081d8775598633ab243861930062d9af6cfd2b
rijinaniyeri <aniyeririjin@gmail.com>
2021-01-20 21:16:38 +0530

    20-10-9:16

---
+++ Screens/Admin/Tournament/ViewTournament/viewTeams.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { firebase } from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
+function selectTournament({ route, navigation }) {
+
+    //const { game } = route.params;
+    const { Dname } = route.params;
+
+
+    const [dataSource, setDataSource] = useState([]);
+
+
+    useEffect(() => {
+
+        firebase.firestore().collection('Team').doc(Dname).collection('phone').onSnapshot(querySnapShot => {
+            const array = [];
+            if (querySnapShot != null) {
+                querySnapShot.forEach(documentSnapShot => {
+                    array.push({
+                        ...documentSnapShot.data()
+                    });
+                    setDataSource(array);
+                })
+            }
+        })
+
+
+    }, [])
+
+
+
+
+    function Item({ data }) {
+        return (
+            <View>
+                <Text>
+                    TEAM NAME : {data.teamName}
+                </Text>
+                <TouchableOpacity onPress={() => { navigation.navigate('teamMembers', { Dname: Dname, Tname: data.teamName }) }}>
+                    <Text>VIEW MEMBERS</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+
+    return (
+        <View>
+            <FlatList
+                data={dataSource}
+                renderItem={({ item }) => <Item data={item} />}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    );
+}
+
+export default selectTournament;
