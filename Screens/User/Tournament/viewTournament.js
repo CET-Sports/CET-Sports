f2b912ad872dc374e3d54eccec1e58bf575e1da7
rijinaniyeri <aniyeririjin@gmail.com>
2021-01-20 15:37:31 +0530

    20-01-21...Tournament

--- Screens/User/Tournament/viewTournament.js
+++ Screens/User/Tournament/viewTournament.js
@@ -1,54 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { View, FlatList, TouchableOpacity, Text } from 'react-native';
-import { firebase } from '@react-native-firebase/app';
-import firestore from '@react-native-firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, FlatList, ScrollView } from 'react-native';
 
-function viewTournament({ navigation }) {
+import { firebase } from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+function ViewTournament({navigation}) {
 
     const [dataSource, setDataSource] = useState([]);
-    const array = [];
 
     useEffect(() => {
+        firebase.firestore().collection('Tournaments').onSnapshot(querySnapshot=>{
+            setDataSource([]);
+            const array = [];
 
-        firebase.firestore().collection('Tournaments').onSnapshot(querySnapShot => {
-
-            if (querySnapShot != null) {
-                querySnapShot.forEach(documentSnapshot => {
-                    array.push({
-                        ...documentSnapshot.id
-                    });
-                    setDataSource(array);
-                    console.log(documentSnapshot.id)
+                firebase.firestore().collection('Tournaments').where('status','==','Pending').get().then(querySnapShot => {
+                    if (querySnapShot != null) {
+                        querySnapShot.forEach(documentSnapshot => {
+                            console.log('documentSnapshot.data()')
+                            console.log(documentSnapshot.data())
+                            array.push({
+                                ...documentSnapshot.data()
+                            });
+                            setDataSource(array);
+                        })
+                    }
                 })
-            }
         })
+
+        
 
     }, [])
 
-
     function Item({ data }) {
         return (
-            <View>
-                <Text>{data[0, 1]}</Text>
-                {/* add tournament name from database above */}
-                <TouchableOpacity onPress={() => { navigation.navigate('viewEvents') }}>
-                    <Text>Enter This Tournament</Text>
-                </TouchableOpacity>
+            <TouchableOpacity  onPress={()=>{navigation.navigate('viewEvents',{
+                Tname:data.name
+            })}}>
+                <Text >{data.name}</Text>
 
-            </View>
+            </TouchableOpacity>
         )
     }
 
+
     return (
-        <View>
             <FlatList
                 data={dataSource}
                 renderItem={({ item }) => <Item data={item} />}
                 keyExtractor={(item, index) => index.toString()}
             />
-
-        </View>
     );
 }
 
-export default viewTournament;
+export default ViewTournament;
