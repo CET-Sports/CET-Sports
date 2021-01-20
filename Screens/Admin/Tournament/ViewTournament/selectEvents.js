ac8315c815202ff1ec8976944f0b48021ef2db09
Ysayoojskiran <ssksayoojskiran@gmail.com>
2021-01-20 20:18:14 +0530

    update on admin select club

--- Screens/Admin/Tournament/ViewTournament/selectEvents.js
+++ Screens/Admin/Tournament/ViewTournament/selectEvents.js
@@ -3,21 +3,39 @@ import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { firebase } from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 
-function selectTournament(props) {
+function selectTournament({ route, navigation }) {
+
+    const { Dname } = route.params;
 
     const [dataSource, setDataSource] = useState([]);
+    const [game, setGame] = useState('');
 
 
-    useEffect(() => { }, [])
+    useEffect(() => {
+
+        firebase.firestore().collection('Team').doc(Dname).collection('phone').onSnapshot(querySnapShot => {
+            const array = [];
+            if (querySnapShot != null) {
+                querySnapShot.forEach(documentSnapShot => {
+                    array.push({
+                        ...documentSnapShot.data()
+                    });
+                    setDataSource(array);
+                })
+            }
+        })
+
+    }, [])
+    setGame(data.Game)
 
 
     function Item({ data }) {
         return (
             <View>
                 <Text>
-                    EVENT NAME :
+                    EVENT NAME : {data.Game}
                 </Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => { navigation.navigate('viewTeams', { game: game }) }}>
                     <Text>ENTER</Text>
                 </TouchableOpacity>
             </View>
